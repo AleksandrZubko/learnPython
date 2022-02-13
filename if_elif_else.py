164b2862fad370c628724257a74082bcbae3452d
Oleksandr Zubko <zubko.alexander@gmail.com>
2022-02-13 23:17:28 +0200

    learn

--- if_elif_else.py
+++ if_elif_else.py
@@ -8,7 +8,7 @@ else:
    print('чёртишо')
 '''
 
-
+'''
 A = int(input())
 B = int(input())
 H = int(input())
@@ -19,3 +19,11 @@ elif H < A:
    print('Недосып')
 elif H > B:
    print('Пересып')
+'''
+
+year = int(input())
+
+if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
+    print('Високосный')
+else:
+    print('Обычный')
---
+++ string.py
@@ -0,0 +1,12 @@
+#string на несколько строк
+c = '''fwds
+dd'''
+print(c);
+
+b = "fg"
+print((b + 'kr') * 3)
+#res = fgkrfgkrfgkr
+
+print(len(b)) #длинна строки
+
+print("123" + "42")
